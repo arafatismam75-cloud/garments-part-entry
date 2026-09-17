bfc2e507f5cc01b46a15e33f270045d030ad941e
MD. ARAFAT ISLAM <arafatismam75@gmail.com>
2026-09-17 14:32:06 +0600

    Add Supabase configuration file

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+// Supabase Configuration
+const SUPABASE_URL = 'https://upcmspspqlwlawncvyre.supabase.co';
+const SUPABASE_ANON_KEY = 'sb_publishable_5FWg_CBzx3vjscLkjP4BGg_42lV4vMv';
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
